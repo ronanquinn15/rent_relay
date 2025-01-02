88a26d084af80339580d0ecd855c26f5a60a8455
Ronan Quinn <ronanq15@gmail.com>
2025-01-02 15:47:00 +0000

    Implement backend landlord features

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
-from backend.blueprints.auth.auth import auth_bp
+from blueprints.auth.auth import auth_bp
+from blueprints.landlord.landlord import landlord_bp
 
 app = Flask(__name__)
 CORS(app)
 app.register_blueprint(auth_bp)
+app.register_blueprint(landlord_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
