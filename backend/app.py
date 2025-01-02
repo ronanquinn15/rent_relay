0d78a5039762a644a13c6d786dc56c1991a9eb6e
Ronan Quinn <ronanq15@gmail.com>
2025-01-02 21:05:18 +0000

    including property blueprint in app.py

--- backend/app.py
+++ backend/app.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_cors import CORS
 from blueprints.auth.auth import auth_bp
 from blueprints.landlord.landlord import landlord_bp
+from blueprints.properties.properties import properties_bp
 
 app = Flask(__name__)
-CORS(app)
+#CORS(app)
 app.register_blueprint(auth_bp)
 app.register_blueprint(landlord_bp)
+app.register_blueprint(properties_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
