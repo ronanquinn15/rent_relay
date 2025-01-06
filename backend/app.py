e6cdd5e7325a52027a7d85f306fc50e949b0cfb8
Ronan Quinn <ronanq15@gmail.com>
2025-01-06 13:35:02 +0000

    Uncommenting CORS for frontend work

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ from blueprints.tenants.tenants import tenants_bp
 from blueprints.maintenance.maintenance import maintenance_bp
 
 app = Flask(__name__)
-#CORS(app)
+CORS(app)
 app.register_blueprint(auth_bp)
 app.register_blueprint(landlord_bp)
 app.register_blueprint(properties_bp)
