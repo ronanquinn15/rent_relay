622a30f72d0ce45f4ba3085f6eba0a0196753134
Ronan Quinn <ronanq15@gmail.com>
2025-01-08 20:58:47 +0000

    Initialising Navigation

---
+++ frontend/src/app/Navigation/nav.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { AuthButtonComponent } from '../AuthenticationButton/authButton.component';
+import { UserAuthComponent } from '../UserAuthentication/userAuth.component';
+import { BehaviorSubject } from 'rxjs';
+import { WebService } from '../WebService/web.service';
+
+@Component({
+  selector: 'navigation',
+  imports: [RouterOutlet, RouterModule, UserAuthComponent, AuthButtonComponent],
+  standalone: true,
+  templateUrl: './nav.component.html'
+})
+export class NavigationComponent {
+  title: string = '';
+  isAuthenticated: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(this.checkAuthStatus());
+
+  constructor(private webService: WebService) {}
+
+  checkAuthStatus(): boolean {
+    return !!sessionStorage.getItem('x-access-token');
+  }
+
+  updateAuthStatus() {
+    this.isAuthenticated.next(this.checkAuthStatus());
+  }
+}
