ea5105d3dcce64970906737de192fd75672883b0
Ronan Quinn <ronanq15@gmail.com>
2025-01-08 20:58:25 +0000

    Updating App.component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from './Navigation/nav.component';
+import { WebService } from './WebService/web.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavigationComponent],
+  providers: [WebService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(private webService: WebService ) { }
+
 }
