ed382f63b88d56240ca34c4899fd453864939e2d
Ronan Quinn <ronanq15@gmail.com>
2025-01-08 20:58:07 +0000

    Updating Routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Home/home.component';
+import { UserAuthComponent } from './UserAuthentication/userAuth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: UserAuthComponent,
+  }
+];
