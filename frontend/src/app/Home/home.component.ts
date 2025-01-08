397625eda0ed3b1d83bbd51f7f950f506b2b7ff9
Ronan Quinn <ronanq15@gmail.com>
2025-01-08 19:38:05 +0000

    Creating home component

---
+++ frontend/src/app/Home/home.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { ColDef } from 'ag-grid-community';
+import { WebService } from '../WebService/web.service';
+import { AgCharts } from 'ag-charts-angular';
+import { AgChartOptions } from 'ag-charts-community';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  standalone: true,
+  imports: [AgGridAngular, AgCharts, FormsModule]
+})
+
+export class HomeComponent {
+
+  /**headings: ColDef[] = [
+    { field: "" },
+    { field: "" },
+    { field: "" },
+    { field: "" },
+    { field: "" },
+    { field: "" },
+    { field: "" },
+  ];*/
+
+  // data: any = [];
+
+  /**gridOptions = {
+    columnDefs: this.headings,
+    rowData: this.filteredData,
+    defaultColDef: {
+      flex: 1,
+      minWidth: 100,
+    },
+  };*/
+
+  constructor(private webService: WebService) { }
+
+
+
+}
