70892058035aa49a06bca0dd73922adff8a19470
Ronan Quinn <ronanq15@gmail.com>
2025-01-12 21:51:52 +0000

    Updating Property UI

--- frontend/src/app/Properties/properties.component.ts
+++ frontend/src/app/Properties/properties.component.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
 import { WebService } from '../WebService/web.service';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-property',
   templateUrl: './properties.component.html',
   styleUrls: ['./properties.component.css'],
-  imports: [RouterOutlet, RouterModule, AsyncPipe],
+  imports: [RouterOutlet, RouterModule, AsyncPipe, CommonModule],
   providers: [WebService],
   standalone: true
 })
-
 export class PropertiesComponent {
   properties: any = [];
 
@@ -23,4 +22,7 @@ export class PropertiesComponent {
     });
   }
 
+  trackById(index: number, prop: any): string {
+    return prop._id;
+  }
 }
