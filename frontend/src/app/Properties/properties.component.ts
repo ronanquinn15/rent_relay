47cb4df4fa25a43f58cd2e239be8488951547226
Ronan Quinn <ronanq15@gmail.com>
2025-03-31 14:59:05 +0100

    Implement pagination for properties in landlord view

--- frontend/src/app/Properties/properties.component.ts
+++ frontend/src/app/Properties/properties.component.ts
@@ -1,28 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { WebService } from '../Services/web.service';
-import { RouterModule, RouterOutlet } from '@angular/router';
-import { AsyncPipe, CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-property',
   templateUrl: './properties.component.html',
   styleUrls: ['./properties.component.css'],
-  imports: [RouterOutlet, RouterModule, AsyncPipe, CommonModule],
+  imports: [RouterModule, CommonModule],
   providers: [WebService],
   standalone: true
 })
-export class PropertiesComponent {
+export class PropertiesComponent implements OnInit {
   properties: any = [];
+  filteredProperties: any = [];
+
+  // Pagination properties
+  currentPage: number = 1;
+  itemsPerPage: number = 5;
+  totalItems: number = 0;
 
   constructor(private webService: WebService) { }
 
   ngOnInit() {
     this.webService.getPropertyWithTenants().subscribe((resp) => {
-        this.properties = resp;
+      this.properties = resp;
+      this.filteredProperties = resp;
+      this.totalItems = resp.length;
     });
   }
 
   trackById(index: number, prop: any): string {
     return prop._id;
   }
+
+  // Pagination methods
+  get paginatedProperties() {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    return this.filteredProperties.slice(startIndex, startIndex + this.itemsPerPage);
+  }
+
+  changePage(page: number) {
+    this.currentPage = page;
+  }
+
+  protected readonly Math = Math;
 }
